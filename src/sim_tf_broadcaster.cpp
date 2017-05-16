cfba600c7bb2ce5393e93168b684b0b0e0c17080
Emir Cem Gezer <emircem.gezer@gmail.com>
2017-05-16 18:38:10 +0300

    some changes in tf

--- src/sim_tf_broadcaster.cpp
+++ src/sim_tf_broadcaster.cpp
@@ -1,8 +1,7 @@
 #include <ros/ros.h>
 #include <tf/transform_broadcaster.h>
 #include <geometry_msgs/Pose2D.h>
-
-// TODO implement a fucking pose publisher for simulation in stage
+#include <nav_msgs/Odometry.h>
 
 std::string robot_name;
 
@@ -18,12 +17,38 @@ void poseCallback(const geometry_msgs::Pose2D& msg){
         tf::StampedTransform(
             transform,
             ros::Time::now(),
-            "base_link",
+            robot_name+"/odom",
             robot_name+"/base_link"
         )
     );
 
 }
+
+void odomCallback(const nav_msgs::Odometry& msg){
+    static tf::TransformBroadcaster br;
+    tf::Transform transform;
+
+    transform.setOrigin(tf::Vector3(msg.pose.pose.position.x, msg.pose.pose.position.y, 0.0) );
+    
+    tf::Quaternion q(
+    msg.pose.pose.orientation.x,
+    msg.pose.pose.orientation.y,
+    msg.pose.pose.orientation.z,
+    msg.pose.pose.orientation.w
+    );
+    transform.setRotation(q);
+    br.sendTransform(
+        tf::StampedTransform(
+            transform,
+            ros::Time::now(),
+            "base_pose_ground_truth",
+            robot_name + "/base_link"    
+
+        )        
+    );
+
+}
+
 
 int main(int argc, char **argv){
     ros::init(argc, argv, "kezbot_tf_broadcaster");
@@ -34,7 +59,7 @@ int main(int argc, char **argv){
     robot_name = argv[1];
 
     ros::NodeHandle node;
-    ros::Subscriber sub = node.subscribe(robot_name+"/pose", 10, &poseCallback);
+    ros::Subscriber sub = node.subscribe(robot_name+"/odom", 10, &odomCallback);
 
     ros::spin();
     return 0;
