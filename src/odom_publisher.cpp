2eac67478278752998d4fe16fa34867f81699213
Emir Cem Gezer <emircem.gezer@gmail.com>
2017-10-03 13:26:27 +0300

    localization is not an issue i guess

--- src/odom_publisher.cpp
+++ src/odom_publisher.cpp
@@ -77,7 +77,7 @@ int main(int argc, char** argv){
         geometry_msgs::TransformStamped odom_trans;
         odom_trans.header.stamp = current_time;
         odom_trans.header.frame_id = "odom";
-        odom_trans.child_frame_id = robot_name+"/base_link";
+        odom_trans.child_frame_id = robot_name+"/base_footprint";
 
          odom_trans.transform.translation.x = x;
          odom_trans.transform.translation.y = y;
